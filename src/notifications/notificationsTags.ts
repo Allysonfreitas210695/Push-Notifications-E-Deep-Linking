dd43562a54776125a6bda0f258700d83f5823d14
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-12-22 19:40:59 -0300

    subindo projeto finalizado

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -6,8 +6,12 @@ import { OneSignal } from "react-native-onesignal";
 
 
 export function tagUserInfoCreate(){
-    OneSignal.User.addTags({
-        user_name: "allyson",
-        user_email: "alisonfr83@gmail.com"
-    });
+    // OneSignal.User.addTags({
+    //     user_name: "allyson",
+    //     user_email: "alisonfr83@gmail.com"
+    // });
+}
+
+export function tagCardUpdate(itemsCount: string) {
+    OneSignal.User.addTag("card_update_count", itemsCount);
 }
